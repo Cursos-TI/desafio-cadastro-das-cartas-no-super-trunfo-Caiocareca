d0e1073355f11e2bbf29e8dab705778385e4f05c
Caiocareca <guimaraesgaldinocaiocesar@gmail.com>
2025-09-02 23:29:24 +0000

    conclusao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,7 @@
 //Teste Caio
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+
     //Carta1
     char Estado1 [60]; //Escolha uma letra de A a H para representar um dos oitos Etados brasileiros
     char Código1 [20]; //Use a letra escolhida seguida de um número de 01 a 04  (ex: A01, B01...)
@@ -27,10 +25,32 @@ int main() {
     float Área2, // Área da cidade em km2
     float PIB2; //Produto interno Bruto da cidade 
     int PontosTurísticos2; //Quantidade de turístas na cidade
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+
+    //Área para entrada de dados
+    printf("Cadastro da carta 1\n");
+
+    printf("Digite o Estado (PB): ");
+    scanf("%s", Estado1);
+
+    printf("Digite o Código (ex: A01): ");
+    scanf("%s", Código1);
+
+    printf("Digite o Nome da Cidade: ");
+    scanf("%[^\n]", NomeCidade1);
+
+printf("Digite a População: "); 
+scanf("%d",&População1);
+
+printf("Digite a Área (km2): ");
+scanf("%f",%Área1); 
+
+print("D9g9te o PIB (bilhões de R$): ");
+scanf("%f", &PIB1):
+
+printf("Digite a quantidade de pontos turísticos: ");
+scanf("%d", &PontosTurísticos1)
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
