38b1f915a6886819b05d1c093ccce2975dc4f845
Caiocareca <guimaraesgaldinocaiocesar@gmail.com>
2025-09-08 21:50:16 +0000

    DESAFIO NIVEL AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,24 +12,28 @@ int main (){
     char Estado1 [60]; //Escolha uma letra de A a H para representar um dos oitos Etados brasileiros
     char Codigo1 [20]; //Use a letra escolhida seguida de um número de 01 a 04  (ex: A01, B01...)
     char NomeCidade1[60]; // Nome da cidade
-    int Populaçao1; //População da cidade
+    float Populaçao1; //População da cidade
     float Area1; //Área da cidade em km2
     float PIB1; //Produto Interno Bruto da cidade
     int PontosTuristicos1; //Quantidade de turístas na cidade
+    float Densipop; // Desensidade populacional
+    float PIP; //Pib percapita
 
     //Carta 2
     char Estado2 [60]; //Escolha uma letra de A a H
     char Codigo2 [20]; //Use a letra escolhida seguida de um número de 01 a 04
     char NomeCidade2 [60]; //Nome da cidade
-    int Populaçao2; //População da cidade
+    float Populaçao2; //População da cidade
     float Area2; // Área da cidade em km2
     float PIB2; //Produto interno Bruto da cidade 
     int PontosTuristicos2; //Quantidade de turístas na cidade
+    float Densipop2; //Densidade populacional
+    float PIP2; //Pib percapita
 
     //Área para entrada de dados
     printf("Cadastro da carta 1\n");
 
-    printf("Digite o Estado (PB): ");
+    printf("Digite o Estado: ");
     scanf("%s", Estado1);
 
     printf("Digite o Codigo (ex: A01): ");
@@ -39,7 +43,7 @@ int main (){
     scanf("%s", NomeCidade1);
 
 printf("Digite a População: \n"); 
-scanf("%d", &Populaçao1);
+scanf("%f", &Populaçao1);
 
 printf("Digite a Área (km2): \n");
 scanf("%f", &Area1); 
@@ -50,9 +54,11 @@ scanf("%f", &PIB1);
 printf("Digite a quantidade de pontos turísticos: \n");
 scanf("%d", &PontosTuristicos1);
 
+Densipop = Populaçao1 / Area1;
+PIP = (PIB1 * 1000000000) / Populaçao1;
 
 printf("Cadastro da carta 2\n");
-printf("Digite o Estado (SP): \n");
+printf("Digite o Estado: \n");
 scanf("%s", Estado2);
 
 printf("Digite o Código (ex:A01): \n");
@@ -62,7 +68,7 @@ printf("Digite o Nome da Cidade: \n");
 scanf("%s", NomeCidade2); 
 
 printf("Digite a População: \n"); 
-scanf("%d", &Populaçao2); 
+scanf("%f", &Populaçao2); 
 
 printf("Digite a Área (km2): \n");
 scanf("%f", &Area2);
@@ -73,45 +79,34 @@ scanf("%f", &PIB2);
 printf("Digite a quantidade de Pontos Turísticos: \n"); 
 scanf("%d", &PontosTuristicos2); 
 
+Densipop2 = Populaçao2 / Area2;
+PIP2 = (PIB2 * 1000000000) / Populaçao2; 
 
 //Área para exibição dos dados das cartas
 
 printf("\n--- Carta 1 ---\n");
 printf("Estado: %s\n", Estado1);
 printf("Código: %s\n", NomeCidade1);
-printf("População: %d\n", Populaçao1);
+printf("População: %.2f\n", Populaçao1);
 printf("Área: %.2f km2\n",Area1);
 printf("PIB: %.2f bilhões de R$\n", PIB1);
 printf("PontosTurísticos: %d\n", PontosTuristicos1); 
+printf("DensidadePopulacional: %.2f\n", Densipop);
+printf("PIB per capita: %.2f\n", PIP);
 printf("----------");
 
 
 printf("\n--- Carta 2 ---\n");
-printf("REstado: %s\n", Estado2);
+printf("Estado: %s\n", Estado2);
 printf("Código: %s\n", Codigo2);
 printf("Nome da Cidade: %s\n", NomeCidade2); 
-printf("População: %d\n", Populaçao2);
+printf("População: %.2f\n", Populaçao2);
 printf("Área: %.2f km2\n", Area2);
 printf("PIB: %.2f bilhões de R$\n", PIB2);
 printf("PontosTurísticos: %d\n", PontosTuristicos2);
+printf("Densidade Populacional: %.2f\n", Densipop2);
+printf("PIP per capita: %.2f\n", PIP2); 
 printf("----------");
-
-
-    int nota1, nota2, nota3;
-    int média;
-    
-    printf("Digite a sua primeira nota: \n");
-    scanf("%d", &nota1);
-
-    printf("Digite sua segunda nota: \n");
-    scanf("%d", &nota2);
-
-    printf("DIgite sua terceira nota: \n");
-    scanf("%d", &nota3);
-
-    média = (nota1 + nota2 + nota3) / 3; 
-
-    printf("A média é: %d", média);
 
     return 0; 
 }
